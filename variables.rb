2df36709ac73eaefdf192d621f1378f64059b211
Xander Hendry <vhendry.coding@gmail.com>
2023-07-28 20:06:09 -0400

    Reasigned dogs and cats, added puts statements

--- variables.rb
+++ variables.rb
@@ -4,3 +4,9 @@ my_siblings = 12
 kaity_siblings = 5
 its_hot = true
 its_cold = false
+dogs = "we don't want any more dogs right now"
+cats = "we want to adopt a cat"
+puts "#{dogs}."
+puts "#{cats}."
+puts "I have #{my_siblings} siblings."
+puts "Kaity has #{kaity_siblings} siblings."
